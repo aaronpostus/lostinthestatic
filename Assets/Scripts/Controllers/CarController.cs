ebded0636340fc2bbea6b6e29225af34799d4b2e
DDDantalion <2020mnie@gmail.com>
2024-01-19 21:32:57 -0500

    suspension

--- Assets/Scripts/Controllers/CarController.cs
+++ Assets/Scripts/Controllers/CarController.cs
@@ -1,17 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using Sirenix.Serialization;
+using Drawing;
 using UnityEngine;
 
-public class CarController : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class CarController : SerializedMonoBehaviour
 {
-    // Start is called before the first frame update
+    [OdinSerialize] Transform[] wheels;
+    private float[] springForces, rollForces, gripForces;
+    [OdinSerialize] LayerMask groundMask;
+    [Header("Suspension Properties")]
+    [OdinSerialize] float suspensionRestDist;
+    [OdinSerialize] float springConst, springDamp;
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        foreach(Transform wheel in wheels)
+        {
+            Vector3 springDir = wheel.up;
+            if (Physics.Raycast(wheel.position, -springDir, out RaycastHit hit, suspensionRestDist, groundMask)) {
+                Vector3 tireVel = rb.GetPointVelocity(wheel.position);
+                float offset = suspensionRestDist - hit.distance;
+                float vel = Vector3.Dot(springDir, tireVel);
+                float force = (offset * springConst) - (vel * springDamp);
+                rb.AddForceAtPosition(springDir * force, wheel.position);
+                
+                Draw.Arrow(wheel.position, wheel.position + springDir, Color.green);
+                Draw.Arrow(wheel.position, wheel.position + springDir, Color.green);
+            }
+        }
+        
     }
 }
