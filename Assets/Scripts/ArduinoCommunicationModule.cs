66b93e3aeec54a32cde2e7293a0c15d7f96dd025
Aaron Post <aarontpost@gmail.com>
2024-01-22 09:56:51 -0500

    In-progress radio tuning

--- Assets/Scripts/ArduinoCommunicationModule.cs
+++ Assets/Scripts/ArduinoCommunicationModule.cs
@@ -3,12 +3,10 @@ using System.IO.Ports;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ArduinoInput : MonoBehaviour
+public class ArduinoCommunicationModule : MonoBehaviour
 {
-    [SerializeField] Slider slider1, slider2;
+    [SerializeField] Radio radio;
     SerialPort data_stream = new SerialPort("COM3", 9600);
-    List<string> testMessages = new List<string>() { "Message One!", "Message Two!", "Message Three!" };
-    public string receivedstring;
     private string receivedBuffer = "";
     // Start is called before the first frame update
     void Start()
@@ -16,16 +14,9 @@ public class ArduinoInput : MonoBehaviour
         data_stream.Open();
         InvokeRepeating("Serial_Data_Reading", 0f, 0.1f);
     }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("A");
-            string message = testMessages[Random.Range(0, testMessages.Count)] + "\n";
-            data_stream.Write(message);
-        }
+    public void SendToLCD(string message) {
+        data_stream.Write(message + "\n");
     }
-
     // Update is called once per frame
     void Serial_Data_Reading()
     {
@@ -34,24 +25,31 @@ public class ArduinoInput : MonoBehaviour
             receivedBuffer += data_stream.ReadExisting();
 
             // Check if the buffer contains a complete message
-            int endIndex = receivedBuffer.IndexOf("$");
-            if (endIndex != -1)
-            {
-                string completeMessage = receivedBuffer.Substring(0, endIndex + 1);
-                receivedBuffer = receivedBuffer.Substring(endIndex + 1);
+            //int endIndex = receivedBuffer.IndexOf("$");
+            //if (endIndex != -1)
+           // {
+                //string completeMessage = receivedBuffer.Substring(0, endIndex + 1);
+                //receivedBuffer = receivedBuffer.Substring(endIndex + 1);
 
                 // Process the complete message
-                ProcessMessage(completeMessage);
-            }
+                ProcessMessage(receivedBuffer);
+            //}
         }
     }
 
     void ProcessMessage(string message)
     {
+        Debug.Log(message);
         string[] datas = message.Split(",");
+        if (datas.Length != 2) {
+            return;
+        }
+        Debug.Log("Tuning to equivalent of input: " + datas[0]);
+        radio.TuneRadio(float.Parse(datas[0]) / 1000.0f);
+        //radio.TuneVolume(float.Parse(datas[1]) / 1000.0f);
         //slider1.value = (float)Mathf.RoundToInt(float.Parse(datas[0])) / 1000.0f;
         //slider2.value = (float)Mathf.RoundToInt(float.Parse(datas[1])) / 1000.0f;
-        Debug.Log(message);
+
     }
 
     private void OnDestroy()
@@ -59,6 +57,7 @@ public class ArduinoInput : MonoBehaviour
         // Close the serial port when the script is destroyed
         if (data_stream.IsOpen)
         {
+            SendToLCD("LCD TURNED OFF");
             data_stream.Close();
         }
     }
--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -1,18 +1,57 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Radio : MonoBehaviour
 {
+    [SerializeField] ArduinoCommunicationModule arduino;
+    [SerializeField] Slider volumeSlider, frequencySlider;
+    public const int maxRadioFreq = 103, minRadioFreq = 93, minVol = 0, maxVol = 38;
+    private int freqRange, volRange;
+    private float frequency = 100, volume = 15;
+    public Radio() {
+        freqRange = maxRadioFreq - minRadioFreq;
+        volRange = (maxVol - minVol) + 1;
+    }
     // volume range is 0f -> 1f
-    public void SetVolume(float vol) { 
-        
+    public void TuneVolume(float value)
+    {
+        // Ensure the input value is between 0 and 1
+        value = Mathf.Max(0, Mathf.Min(1, value));
+        // Map the input value to the integer range
+        int result = minVol + (int)(value * volRange);
+        SetVolume(result);
     }
     // tune to an appropriate station from input between 0f - 1f
-    public void TuneRadio(float input) { 
-    
+    public void TuneRadio(float value)
+    {
+        // Ensure the input value is between 0 and 1
+        value = Mathf.Max(0, Mathf.Min(1, value));
+        // Map the input value to the output range
+        float freq = minRadioFreq + value * freqRange;
+        SetFrequency(freq);
     }
     // set radio to an exact frequency (i.e. 99.5f)
-    public void SetFrequency(float freq) { 
-    
+    private void SetFrequency(float freq) {
+        // don't do anything if the frequency hasn't actually changed
+        if (this.frequency == freq) {
+            return;
+        }
+        this.frequency = freq;
+        RefreshFrequencyLCD();
+    }
+    // set radio to an exact frequency (i.e. 99.5f)
+    private void SetVolume(float vol)
+    {
+        // don't do anything if the volume hasn't actually changed
+        if (this.volume == vol) {
+            return;
+        }
+        this.volume = vol;
+
+    }
+    // updates the controller LCD
+    private void RefreshFrequencyLCD() {
+        arduino.SendToLCD(frequency + "");
     }
     // Start is called before the first frame update
     void Start()
