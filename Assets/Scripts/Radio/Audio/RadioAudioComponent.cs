99362deea2a60250d1cf3c5058152dfd28daaa02
Aaron Post <aarontpost@gmail.com>
2024-01-31 13:53:34 -0500

    Created radio audio branch

---
+++ Assets/Scripts/Radio/Audio/RadioAudioComponent.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Radio.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RadioAudioComponent
+{
+    private Dictionary<float, RadioChannel> radioChannels;
+    public void Seek(float radioChannel) { 
+    
+    }
+}
