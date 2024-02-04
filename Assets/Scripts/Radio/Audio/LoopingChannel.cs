4cb965f2607329fbee65f7aae8b0f0ca6474d7dd
Aaron Post <aarontpost@gmail.com>
2024-02-03 21:18:06 -0500

    commits 4 mathew

--- Assets/Scripts/Radio/Audio/LoopingChannel.cs
+++ Assets/Scripts/Radio/Audio/LoopingChannel.cs
@@ -1,46 +1,42 @@
 ﻿using FMOD.Studio;
 using UnityEngine;
 
+// A looping channel will start from the beginning the first time the user seeks to it.
+// Then, while they are selected on that station, it will continue looping.
+// If the user seeks away, it will continue to the end of the audio file then wait for the user to seek back before restarting.
+// 
+// This channel is intended to be used for narrative heavy channels rather than something like music.
 public class LoopingChannel : IRadioChannel
 {
     EventInstance loopEventInstance;
     public LoopingChannel(string fmodEventDir, GameObject attentuationObject) { 
         this.loopEventInstance = FMODUnity.RuntimeManager.CreateInstance(fmodEventDir);
         this.loopEventInstance.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(attentuationObject));
+
     }
     public void SeekTo() {
         Debug.Log("Seek to looping channel");
-        loopEventInstance.start();
+
+        PLAYBACK_STATE playbackState;
+        loopEventInstance.getPlaybackState(out playbackState);
+        loopEventInstance.setVolume(1.0f);
+        if (playbackState == PLAYBACK_STATE.STOPPED)
+        {
+            // Handle the event stopping here
+            Debug.Log("Last time the loop stopped! Starting from beginning");
+            loopEventInstance.start();
+            // You can perform any additional actions or logic here
+        }
+        loopEventInstance.setParameterByName("LOOP",0f);
+        loopEventInstance.getParameterByName("LOOP", out var value);
+        Debug.Log("LOOP:"+value);
     }
     public void SeekAwayFrom() {
-        //loopEventInstance.
         Debug.Log("Seek away from looping channel");
-        loopEventInstance.stop(STOP_MODE.IMMEDIATE);
+        loopEventInstance.setVolume(0.0f);
+        loopEventInstance.setParameterByName("LOOP", 0f);
+        loopEventInstance.getParameterByName("LOOP", out var value);
+        Debug.Log("LOOP:"+value);
     }
-    public void Update()
-    {
-        // Check if the sound has reached the end
-        if (IsSoundPlaying() && !IsSoundPlayingInLoop())
-        {
-            // Restart the sound to loop it
-            loopEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-            loopEventInstance.start();
-        }
-    }
-
-    bool IsSoundPlaying()
-    {
-        FMOD.Studio.PLAYBACK_STATE playbackState;
-        loopEventInstance.getPlaybackState(out playbackState);
-        return playbackState == FMOD.Studio.PLAYBACK_STATE.PLAYING;
-    }
-
-    bool IsSoundPlayingInLoop()
-    {
-        //bool isLooping;
-        //loopEventInstance.get3DAttributes(out isLooping);
-        // return isLooping;
-        return true;
-    }
-
+    // ideally this would be done with a callback instead of this way but fmod documentation kinda sucks
 }
