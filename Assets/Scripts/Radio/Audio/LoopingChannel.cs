e7e0d75607422b32f0c721dbc304fb9bab5d84fc
Aaron Post <aarontpost@gmail.com>
2024-02-04 22:15:48 -0500

    Woooo working cool looping audio channels

--- Assets/Scripts/Radio/Audio/LoopingChannel.cs
+++ Assets/Scripts/Radio/Audio/LoopingChannel.cs
@@ -16,28 +16,20 @@ public class LoopingChannel : IRadioChannel
 
     }
     public void SeekTo() {
-        Debug.Log("Seek to looping channel");
-
         PLAYBACK_STATE playbackState;
         loopEventInstance.getPlaybackState(out playbackState);
         loopEventInstance.setVolume(1.0f);
         if (playbackState == PLAYBACK_STATE.STOPPED)
         {
-            // Handle the event stopping here
             Debug.Log("Last time the loop stopped! Starting from beginning");
             loopEventInstance.start();
-            // You can perform any additional actions or logic here
         }
         loopEventInstance.setParameterByName("LOOP",1);
         loopEventInstance.getParameterByName("LOOP", out var value);
         Debug.Log("LOOP:"+value);
     }
     public void SeekAwayFrom() {
-        Debug.Log("Seek away from looping channel");
         loopEventInstance.setVolume(0.0f);
         loopEventInstance.setParameterByName("LOOP", 0f);
-        loopEventInstance.getParameterByName("LOOP", out var value);
-        Debug.Log("LOOP:"+value);
     }
-    // ideally this would be done with a callback instead of this way but fmod documentation kinda sucks
 }
