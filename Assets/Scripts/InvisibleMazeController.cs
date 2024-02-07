2f8c3eac7530178dfd699d5bd998864899b9f819
DDDantalion <2020mnie@gmail.com>
2024-02-07 03:56:31 -0500

    maze fix? and tuning and build zip

--- Assets/Scripts/InvisibleMazeController.cs
+++ Assets/Scripts/InvisibleMazeController.cs
@@ -10,31 +10,60 @@ public class InvisibleMazeController : MonoBehaviour
     [SerializeField] Transform respawnPosition;
     [SerializeField] float maxDistanceBeforeDeath;
     private InvisibleMazeChannel channel;
-    public void StartTrackingPlayer() {
-        UpdateTicker.Subscribe(UpdatePlayerPos);
-    }
-    public void StopTrackingPlayer() { 
-        UpdateTicker.Unsubscribe(UpdatePlayerPos);
-    }
-    public void PassChannel(InvisibleMazeChannel channel) {
-        this.channel = channel;
-    }
-    void UpdatePlayerPos() {
+    private bool trackPlayer;
+
+    private void FixedUpdate()
+    {
+        if (!trackPlayer) return;
+
         SplineSample sample = new SplineSample();
         Vector3 playerPos = player.transform.position;
         spline.Project(playerPos, ref sample);
         Vector3 pointPos = sample.position;
         float distance = Mathf.Sqrt(Mathf.Pow(playerPos.x - pointPos.x, 2) + Mathf.Pow(playerPos.z - pointPos.z, 2));
         Debug.Log(distance);
-        channel.UpdateBalance(1-(distance / maxDistanceBeforeDeath));
-        if (distance > maxDistanceBeforeDeath) {
+        channel.UpdateBalance(1 - (distance / maxDistanceBeforeDeath));
+        if (distance > maxDistanceBeforeDeath)
+        {
             Lose();
         }
     }
 
+
+    public void StartTrackingPlayer() {
+        trackPlayer = true;
+    }
+
+    public void StopTrackingPlayer() {
+        trackPlayer = false;
+    }
+
+    public void PassChannel(InvisibleMazeChannel channel) {
+        this.channel = channel;
+    }
+
     void Lose() {
+        
+
+        StartCoroutine(ReleaseOnNextStep());
+    }
+
+    private IEnumerator ReleaseOnNextStep() {
+        Rigidbody rb = player.transform.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        
+        rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+        rb.interpolation = RigidbodyInterpolation.None;
+        yield return new WaitForFixedUpdate();
         player.transform.rotation = respawnPosition.rotation;
-        player.GetComponent<Rigidbody>().velocity = Vector3.zero;
         player.transform.position = respawnPosition.position;
+        rb.angularVelocity = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        rb.isKinematic = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        rb.interpolation = RigidbodyInterpolation.Interpolate;
     }
 }
