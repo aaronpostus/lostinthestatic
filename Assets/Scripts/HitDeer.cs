f37f026bf9632346b4ab2a9be0a520e223a57bee
Micah Glus <mglus01@hotmail.com>
2024-02-15 23:56:55 -0500

    deer gets hit

--- Assets/Scripts/HitDeer.cs
+++ Assets/Scripts/HitDeer.cs
@@ -10,12 +10,19 @@ public class HitDeer : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            numHits += 1;
+            Debug.Log(col.GetComponent<Rigidbody>().velocity.magnitude);
+            if (col.GetComponent<Rigidbody>().velocity.magnitude >= 10)
+            {
+                numHits += 1;
+            }
         }
     }
 
     void Update()
     {
-        
+        if (numHits >= 3)
+        {
+            Destroy(gameObject);
+        }
     }
 }
