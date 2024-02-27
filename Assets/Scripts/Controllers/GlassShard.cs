73df4c4868fbab6479019d745cca82946ab89570
Aaron Post <aarontpost@gmail.com>
2024-02-27 01:00:13 -0500

    fdasfdas

--- Assets/Scripts/Controllers/GlassShard.cs
+++ Assets/Scripts/Controllers/GlassShard.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
 
 public class GlassShard : MonoBehaviour, IInteractable
 {
     public InteractionType Type => InteractionType.Inspect;
     [SerializeField] GlassShardHUD hud;
     [SerializeField] Sprite sprite;
-
+    [SerializeField] bool isHUD;
     public void Interact()
     {
         hud.DisplayShard(sprite);
--- Assets/Scripts/GlassShardHUD.cs
+++ Assets/Scripts/GlassShardHUD.cs
@@ -13,7 +13,10 @@ public class GlassShardHUD : MonoBehaviour
     [SerializeField] GameObject glassShardView;
     [SerializeField] Image shardHUD;
     [SerializeField] TextMeshProUGUI stopViewingShard;
+    [SerializeField] RawImage hud1;
+    [SerializeField] Image hud2;
     bool displayingShard = false;
+    bool displayingImage = false;
     private void Awake()
     {
         stopViewingShard.color = new Color(stopViewingShard.color.r, stopViewingShard.color.g, stopViewingShard.color.b, 0f);
@@ -40,6 +43,71 @@ public class GlassShardHUD : MonoBehaviour
         displayingShard = true;
         shardHUD.sprite = shardSprite;
         StartCoroutine(FadeIn());
+    }
+    public void DisplayMap() {
+        if (displayingShard) {
+            return;
+        }
+        displayingShard = true;
+        displayingImage = true;
+        StartCoroutine(FadeInImage());
+    }
+    IEnumerator FadeInImage()
+    {
+        float elapsedTime = 0f;
+        float duration = 1.5f;
+        Color startColor = hud1.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 1f); // Target color with alpha 1
+
+        Color startColor2 = hud2.color;
+        Color targetColor2 = new Color(startColor2.r, startColor2.g, startColor2.b, 1f); // Target color with alpha 1
+
+        Color startColor3 = stopViewingShard.color;
+        Color targetColor3 = new Color(startColor3.r, startColor3.g, startColor3.b, 1f); // Target color with alpha 1
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            hud1.color = Color.Lerp(startColor, targetColor, elapsedTime / duration);
+            hud2.color = Color.Lerp(startColor2, targetColor2, elapsedTime / duration);
+            stopViewingShard.color = Color.Lerp(startColor3, targetColor3, elapsedTime / duration);
+            yield return null;
+
+            // Check if we need to stop fading
+            if (!displayingShard)
+            {
+                yield break; // Exit the coroutine early
+            }
+        }
+    }
+    IEnumerator FadeOutImage()
+    {
+        float elapsedTime = 0f;
+        float duration = 1.5f;
+
+        Color startColor = hud1.color;
+        Color targetColor = new Color(startColor.r, startColor.g, startColor.b, 0f); // Target color with alpha 1
+
+        Color startColor2 = hud2.color;
+        Color targetColor2 = new Color(startColor2.r, startColor2.g, startColor2.b, 0f); // Target color with alpha 1
+
+        Color startColor3 = stopViewingShard.color;
+        Color targetColor3 = new Color(startColor3.r, startColor3.g, startColor3.b, 0f); // Target color with alpha 0
+
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            stopViewingShard.color = Color.Lerp(startColor, targetColor, elapsedTime / duration);
+            shardHUD.color = Color.Lerp(startColor2, targetColor2, elapsedTime / duration);
+            stopViewingShard.color = Color.Lerp(startColor3, targetColor3, elapsedTime / duration);
+            yield return null;
+
+            // Check if we need to stop fading
+            if (displayingShard)
+            {
+                yield break; // Exit the coroutine early
+            }
+        }
     }
     IEnumerator FadeIn()
     {
@@ -92,6 +160,11 @@ public class GlassShardHUD : MonoBehaviour
 
     void StopDisplayingShard() {
         displayingShard = false;
+        if (displayingImage) {
+            displayingImage = false;
+            StartCoroutine(FadeOutImage());
+            return;
+        }
         StartCoroutine(FadeOut());
     }
     public void Update()
